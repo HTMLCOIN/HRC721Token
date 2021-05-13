43a33bdbc98d1164b593b0a1bfe4095d0a219445
kevin <incredibledev.sec@gmail.com>
2021-05-13 15:36:39 -0400

    polish up smart contracts

--- contracts/HRC721.sol
+++ contracts/HRC721.sol
@@ -1,10 +1,33 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0;
 
-import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721Enumerable.sol";
-import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721MetadataMintable.sol";
 import "./IHRC721.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
+import "./MinterRole.sol";
 
-contract HRC721 is IHRC721, ERC721Enumerable, ERC721MetadataMintable {
-    constructor(string memory name, string memory symbol) ERC721Metadata(name, symbol) public {
+contract HRC721 is
+    ERC721,
+    ERC721Enumerable,
+    ERC721URIStorage,
+    IHRC721,
+    MinterRole
+{
+    constructor(string memory name, string memory symbol)
+        public
+        ERC721(name, symbol)
+    {}
+
+    function mintWithTokenURI(
+        address to,
+        uint256 tokenId,
+        string memory tokenURI
+    ) public onlyMinter returns (bool) {
+        _mint(to, tokenId);
+        _setTokenURI(tokenId, tokenURI);
+        return true;
     }
+
+    //TODO:Kevin Can be able to add custom functions
 }
--- contracts/IHRC721.sol
+++ contracts/IHRC721.sol
@@ -1,38 +1,21 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0;
 
-contract IHRC721 {
-    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
-    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
-    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/IERC721.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/extensions/IERC721Metadata.sol";
 
-    function supportsInterface(bytes4 interfaceId) external view returns (bool);
+interface IHRC721 is IERC721, IERC721Enumerable, IERC721Metadata {
+    //  Basically, default functions are inherited from ERC721
+    function isMinter(address account) external view returns (bool);
 
-    function name() external view returns (string memory);
-    function symbol() external view returns (string memory);
-    function tokenURI(uint256 tokenId) external view returns (string memory);
+    function addMinter(address account) external;
 
-    function balanceOf(address owner) public view returns (uint256 balance);
-    function ownerOf(uint256 tokenId) public view returns (address owner);
+    function renounceMinter() external;
 
-    function approve(address to, uint256 tokenId) public;
-    function getApproved(uint256 tokenId) public view returns (address operator);
-
-    function setApprovalForAll(address operator, bool _approved) public;
-    function isApprovedForAll(address owner, address operator) public view returns (bool);
-
-    function transferFrom(address from, address to, uint256 tokenId) public;
-    function safeTransferFrom(address from, address to, uint256 tokenId) public;
-
-    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public;
-
-    function totalSupply() public view returns (uint256);
-    function tokenOfOwnerByIndex(address owner, uint256 index) public view returns (uint256 tokenId);
-
-    function tokenByIndex(uint256 index) public view returns (uint256);
-
-    function isMinter(address account) public view returns (bool);
-    function addMinter(address account) public;
-    function renounceMinter() public;
-
-    function mintWithTokenURI(address to, uint256 tokenId, string memory tokenURI) public returns (bool);
+    function mintWithTokenURI(
+        address to,
+        uint256 tokenId,
+        string memory tokenURI
+    ) external returns (bool);
 }
